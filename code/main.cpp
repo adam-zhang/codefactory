d85dc476c749bb689aca1e59c32b1fb8e8fde6fd
adam-zhang <mingpingzhang@163.com>
2021-01-26 16:06:17 +0800

    the pramamenters out of paramemtenrs added.

--- code/main.cpp
+++ code/main.cpp
@@ -13,23 +13,28 @@ int main(int argc, char** argv)
 		Logger::output(Notice::usage());
 		return 0;
 	}
-	if (analyzer.argument(1) == "--help")
+	else if (analyzer.argument(1) == "--help")
 	{
 		Logger::output(Notice::usage());
 		return 0;
 	}
-	if (analyzer.argument(1) == "--class")
+	else if (analyzer.argument(1) == "--class")
 	{
 		generateClass(analyzer.argument(2));
 		return 0;
 	}
-	if (analyzer.argument(1) == "--project")
+	else if (analyzer.argument(1) == "--project")
 	{
 		generateProject(analyzer.argument(2));
 	}
-	if (analyzer.argument(1) == "--qtproject")
+	else if (analyzer.argument(1) == "--qtproject")
 	{
 		generateQtProject(analyzer.argument(2));
 	}
+	else
+	{
+		Logger::output(Notice::usage());
+	}
+
 	return 0;
 }

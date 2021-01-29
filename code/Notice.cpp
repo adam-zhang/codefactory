a20e4bd4a55f2a42d66f15d1484dd58ebe7710d9
adam-zhang <mingpingzhang@163.com>
2021-01-30 00:02:10 +0800

    a little change

--- code/Notice.cpp
+++ code/Notice.cpp
@@ -5,10 +5,10 @@ using namespace std;
 string Notice::usage()
 {
 	return "Usage: codefactory --arguments\n"
-		"\t--class <class_name>	produce a class which named class_name\n"
-		"\t--project 		produce a simple project which contains files main.cpp and CMakeFile.txt\n"
-		"\t--singleton		produce a file contains a singleton class\n"
-		"\t--threadpool		produce the files for thread pool\n"
-		"\t--properties		produce a file contains a macro which can produce a property simply."
-		"\t--queue		produce a file onntains a thread safe queue.";
+		"\t--class <class_name>		produce a class which named class_name\n"
+		"\t--project <project_name>	produce a simple project which contains files main.cpp and CMakeFile.txt\n"
+		"\t--singleton			produce a file contains a singleton class\n"
+		"\t--threadpool			produce the files for thread pool\n"
+		"\t--properties			produce a file contains a macro which can produce a property simply.\n"
+		"\t--queue				produce a file onntains a thread safe queue.";
 }
